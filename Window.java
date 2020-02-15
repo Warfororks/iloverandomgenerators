751fd381095ec9d5ad80eff1c70871577aa5210e
Colin Pietropaolo <spy611@yahoo.com>
2020-02-15 16:20:07 -0500

    final working version

--- Window.java
+++ Window.java
@@ -10,6 +10,8 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javafx.*;
+
 
 
 
@@ -18,7 +20,8 @@ public class Window extends JPanel implements Runnable, KeyListener{
 	private static int WIDTH;
 	private static int HEIGHT;
 	private static boolean running = false;
-	private static int fps = 60;
+	private static boolean start = false;
+	private static int fps = 20;
 	private static int maintime = 1000/ fps;
 	private Floor floor;
 	private Player p;
@@ -45,7 +48,21 @@ public class Window extends JPanel implements Runnable, KeyListener{
 		blocks[2] = new Enemy(1850, h-120, 40, 40, new File("src/block.png"));
 		blocks[3] = new Enemy(2000, h-120, 40, 40, new File("src/block.png"));
 		blocks[4] = new Enemy(2000, h-160, 40, 40, new File("src/block.png"));
-		blocks[19] = new Enemy(2400, h-120, 40, 40, new File("src/block.png"));
+		blocks[5] = new Enemy(2200, h-120, 40, 40, new File("src/block.png"));
+		blocks[6] = new Enemy(2350, h-120, 40, 40, new File("src/block.png"));
+		blocks[7] = new Enemy(2350, h-160, 40, 40, new File("src/block.png"));
+		blocks[8] = new Enemy(2350, h-200, 40, 40, new File("src/block.png"));
+		blocks[9] = new Enemy(2500, h-120, 40, 40, new File("src/block.png"));
+		blocks[10] = new Enemy(2650, h-120, 40, 40, new File("src/block.png"));
+		blocks[11] = new Enemy(2750, h-120, 40, 40, new File("src/block.png"));
+		blocks[12] = new Enemy(2750, h-160, 40, 40, new File("src/block.png"));
+		blocks[13] = new Enemy(2850, h-120, 40, 40, new File("src/block.png"));
+		blocks[14] = new Enemy(2850, h-160, 40, 40, new File("src/block.png"));
+		blocks[15] = new Enemy(2850, h-200, 40, 40, new File("src/block.png"));
+		blocks[16] = new Enemy(3000, h-120, 40, 40, new File("src/block.png"));
+		blocks[17] = new Enemy(3000, h-160, 40, 40, new File("src/block.png"));
+		blocks[18] = new Enemy(3000, h-200, 40, 40, new File("src/block.png"));
+		blocks[19] = new Enemy(3000, h-240, 40, 40, new File("src/block.png"));
 
 		this.setBackground(Color.BLACK);
 		floor.resizeImage(w+10, 100);
@@ -59,10 +76,13 @@ public class Window extends JPanel implements Runnable, KeyListener{
 		
 		while(running) {
 			long startT = System.nanoTime();
-			//update();
-			//render();
-			//System.out.println(isJumping);
-//			
+			if(start) {
+				for(int i = 0; i < 20; i++) {
+					if(blocks[i] != null) blocks[i].setX(blocks[i].getX()-10);
+				}
+			}
+			isCollisionWithBlock(p);
+			update();
 			updateJump();
 			repaint();
 			long endT = System.nanoTime() - startT;
@@ -91,18 +111,18 @@ public class Window extends JPanel implements Runnable, KeyListener{
 			isJumping = false;
 			isFalling = true;
 		}
-		else if(isFalling && height < 31) {
+		else if(isFalling && height < 30) {
 			p.setY(p.getY() + 10);
 			height++;
 		}
-		else if(isFalling && height == 31) {
+		else if(isFalling && height == 30) {
 			isFalling = false;
 			height = 0;
 		}
+	}
 	
 	public void update() {
 		if(p.getX() > blocks[19].getX()+blocks[19].getWidth()) {
-			System.out.println("you win");
 			try {
 				end = ImageIO.read(new File("src/youwin.png"));
 				running = false;
@@ -140,47 +160,36 @@ public class Window extends JPanel implements Runnable, KeyListener{
 		if(p != null) {
 			g.drawImage(p.getImage(), (int)p.getX(), (int)p.getY(), this);
 		}
-			g.drawImage(floor.getImage(), (int)floor.getX(), (int)floor.getY(), this);
+		if(end != null) g.drawImage(end, 0, 0, this);
+		g.drawImage(floor.getImage(), (int)floor.getX(), (int)floor.getY(), this);
 		for(int i = 0; i < 20; i++) {
 			if(blocks[i] != null) {
 				g.drawImage(blocks[i].getImage(), (int)blocks[i].getX(), (int)blocks[i].getY(), this);
 			}
 		}
-		if(end != null)
-			g.drawImage(end, 0, 0, this);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		removeAll();
-		for(int i = 0; i < 20; i++) {
-			if(blocks[i] != null) blocks[i].setX(blocks[i].getX()-10);
-		}
+		start = true;
 		int code = arg0.getKeyCode();
 		switch(code) {
 			case KeyEvent.VK_UP:
 				if(!isCollisionWithWall(p)) {
-					p.setY(p.getY() - 10);
+				//	p.setY(p.getY() - 10);
 					jump();
 				}
-				//move up
-				//System.out.println("move up");
 				break;
 			case KeyEvent.VK_LEFT:
 				if(!isCollisionWithWall(p))
 					p.setX(p.getX() - 10);
 					p.setImage(leftbron);
-					
-				//move left
-				//System.out.println("move left");
 				break;
 			case KeyEvent.VK_RIGHT:
 				if(!isCollisionWithWall(p))
 				p.setX(p.getX() + 10);
 				p.setImage(rightbron);
-		
-				//move right
-				//System.out.println("move right");
 				break;
 			case KeyEvent.VK_DOWN:
 				if(!isCollisionWithWall(p))
@@ -191,7 +200,7 @@ public class Window extends JPanel implements Runnable, KeyListener{
 
 	public boolean isCollisionWithWall(Player p) {
 		boolean cantMove = false;
-		if(p.getX() == 100) {
+		if(p.getX() <= 100) {
 			cantMove = true;
 			endScreen();
 			p.setX(p.getX() + 10);
@@ -211,6 +220,27 @@ public class Window extends JPanel implements Runnable, KeyListener{
 			//System.out.println(this.getParent().getHeight());
 		}
 			
+		return cantMove;
+	}
+	
+	public boolean isCollisionWithBlock(Player p) {
+		boolean cantMove = false;
+		for(int i = 0; i < 20; i++) {
+			if(blocks[i] == null) continue;
+			if(blocks[i].getX() > 100 && blocks[i].getX() < 1220) {
+				if(blocks[i].getX() > p.getX()-20 && blocks[i].getX() < p.getX()+p.getWidth()+30) {
+					if(blocks[i].getY() <= p.getY()+p.getLength()) {
+						p.setX(p.getX()-20);
+						//p.setY(p.getY()+1);
+						cantMove = true;
+					}
+				}
+			} 
+		}
+		if(p.getX() <= 100) {
+			cantMove = true;
+			endScreen();
+		}
 		return cantMove;
 	}
 	
@@ -253,6 +283,7 @@ public class Window extends JPanel implements Runnable, KeyListener{
 	public void endScreen() {
 		try {
 			end = ImageIO.read(new File("src/gameover.png"));
+			repaint();
 			running = false;
 		}
 		catch (IOException e) {
